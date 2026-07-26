06b0d34ccb7dd25dabdf77c58ee0c262479dc77f
mau <onestepright8@gmail.com>
2026-07-26 03:18:34 +0000

    feat: add application w db and ingress/tls configs

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -35,12 +35,6 @@ resource "azurerm_subnet" "aks" {
   address_prefixes     = [var.aks_subnet_cidr]
 }
 
-resource "azurerm_network_security_group" "aks" {
-  name                = "nsg-aks-${var.prefix}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-}
-
 resource "azurerm_subnet_network_security_group_association" "aks" {
   subnet_id                 = azurerm_subnet.aks.id
   network_security_group_id = azurerm_network_security_group.aks.id
@@ -52,4 +46,34 @@ output "aks_subnet_id" {
 
 output "vnet_id" {
   value = azurerm_virtual_network.this.id
+}
+
+resource "azurerm_network_security_group" "aks" {
+  name                = "nsg-aks-${var.prefix}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "AllowHTTPInbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTPSInbound"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
 }
